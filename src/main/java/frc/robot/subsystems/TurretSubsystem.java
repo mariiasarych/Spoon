759ae2c2d1ae8b54ce7d66a86c38c5c428f226aa
mariiasarych <57046693+mariiasarych@users.noreply.github.com>
2020-02-16 15:02:52 -0500

    Had issues with git. Turret sub. is made by Sofia!

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,66 +1,56 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
+
+
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
-
-import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+/**
+ * Add your docs here.
+ */
 public class TurretSubsystem extends SubsystemBase {
-
-  public WPI_TalonSRX m_turret;
-  AnalogInput encoder;
-  double encoderValue;
-  double currentAngle;
-  double maxspeed;
-  TurretSubsystem m_turretSystem;
-  /**
-   * Creates a new TurretSubsystem.
-   */
-  public TurretSubsystem() {
-    m_turret = new WPI_TalonSRX(6);
-    encoder = new AnalogInput(1); //not the correct port
-  }
-
-  public void setMaxSpeed () {
-    m_turret.set(.20);
-  }
+  WPI_TalonSRX m_turret;
+  WPI_TalonSRX m_feeder;
+  WPI_TalonFX m_shooterLeft;
+  WPI_TalonFX m_shooterRight;
   
-  public void setMinSpeed () {
-    m_turret.set(-.20);
+  public TurretSubsystem(){
+    m_turret = new WPI_TalonSRX(6);
+    m_feeder = new WPI_TalonSRX(5);
+    m_shooterLeft = new WPI_TalonFX(7);
+    m_shooterRight = new WPI_TalonFX(8);
+    boolean _brake = true;
+    m_shooterLeft.setNeutralMode(NeutralMode.Brake);
+    m_shooterRight.setNeutralMode(NeutralMode.Brake);
   }
 
-  public void setTurretSpeed (double t_speed) {
-    //here must be the formula that would make joystick values go from -.20 to .20
-    if (t_speed > 1){
-      t_speed = 1;
+  public void setTurretSpeed(double speed){ //sets and regulates turret speed
+    if (speed > 1) {
+      speed = 1;
     }
-    else if (t_speed < -1){
-      t_speed = - 1;
+    else if (speed < -1) {
+      speed = -1;
     }
-    t_speed = t_speed *.20;
-    m_turret.set (t_speed);
+    m_turret.set(speed*0.25); 
   }
 
-  public void resetTurretEncoder () { //probably should be in the encoder subsystem
-    encoder.resetAccumulator();
+  public double encoderVal(){ //gets the value of the turret magnetic encoder
+    double position = m_turret.getSelectedSensorPosition(0);
+    System.out.println("encoder position " + position);
+    return position;
   }
 
-  public double getCurrentAngle (TurretSubsystem turret_subsystem){
-    //here must be a formula that will ensure that encoder's values are accurate
-    currentAngle = turret_subsystem.encoder.getVoltage();
-    return currentAngle;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  
 }
